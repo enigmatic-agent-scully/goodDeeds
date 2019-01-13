b1feef01bfbc16057ba392aa2d7c8e688da63e7d
spinozist <ewoodworth@atlantaregional.org>
2019-01-13 18:18:26 -0500

    added sourceType: module to eslint

--- good-deeds-react/src/App.js
+++ good-deeds-react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Landing from "./components/Pages/Landing"
+import Landing from './components/Pages/Landing';
 
 class App extends Component {
   render() {
     return (
+      <Landing />
     );
   }
 }
