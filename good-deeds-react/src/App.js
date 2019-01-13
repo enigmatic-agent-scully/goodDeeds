6b19ee5724db99fd33b84ecdd92e13f3eb753486
spinozist <ewoodworth@atlantaregional.org>
2019-01-12 21:14:51 -0500

    created React boilerplate and component folder tree

---
+++ good-deeds-react/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Landing from "./components/Pages/Landing"
+
+class App extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default App;
