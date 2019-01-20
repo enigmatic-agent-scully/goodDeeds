69f2b7b686a676d19915b1d7b1aafd5a443ff7a8
AJ <amanda.elisabet.jordan@gmail.com>
2019-01-20 16:41:46 -0500

    api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
 export default {
-  // Get all deeds
-  getDeeds: () => {
-    return axios.get('/api/deeds');
+  // Get all needs
+  getNeeds: () => {
+    return axios.get('/api/needs');
   },
 
-  // Get deed by id
-  getDeed: id => {
-    return axios.get(`/api/deeds/${id}`);
+  // Get need by id
+  getNeed: id => {
+    return axios.get(`/api/needs/${id}`);
   },
 
-  // Post a new deed
-  postDeed: deedData => {
-    return axios.post(`/api/deeds`, deedData);
+  // Post a new need
+  postNeed: needData => {
+    return axios.post(`/api/needs`, needData);
   },
 
-  // Change the status of deed
+  // Change the status of need
   markComplete: id => {
-    return axios.put(`/api/deeds/${id}`, {
+    return axios.put(`/api/needs/${id}`, {
       completed: true
     });
   },
 
   // Need to check if this the correct way to organize all the messages for one post
   postMessage: (id, messageData) => {
-    return axios.put(`/api/deeds/${id}/messages`, messageData);
+    return axios.put(`/api/needs/${id}/messages`, messageData);
   }
 };
