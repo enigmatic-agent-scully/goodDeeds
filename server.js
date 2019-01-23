78d37a7fae73258cc8f1e3bef119f1de18bd4d8b
spinozist <ewoodworth@atlantaregional.org>
2019-01-23 15:14:31 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static(__dirname + '/'));
 }
 
 // Incorporate PASSPORT
