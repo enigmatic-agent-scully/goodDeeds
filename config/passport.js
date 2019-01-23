40e48c8473861e39b8681649c64acd0f52ee3363
mikeB <mike.f.bagheri@gmail.com>
2019-01-22 21:20:37 -0500

    reading/writing to dB, and creating a session

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose')
-var User = mongoose.model('user');
+var User = mongoose.model('User');
 // const db = require("./../models");
 
 
