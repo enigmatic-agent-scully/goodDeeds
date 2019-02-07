885fcf4bb226576eeb6ed4885bea944787a26b85
AJ <amanda.elisabet.jordan@gmail.com>
2019-02-07 14:44:47 -0500

    font face and button color changes

--- client/src/components/Pages/Landing/index.js
+++ client/src/components/Pages/Landing/index.js
@@ -5,52 +5,53 @@ import Login from '../../Tools/Login';
 import './style.css';
 
 const insideStyles = {
-
   position: 'absolute',
   top: '50%',
   left: '50%',
-  transform: 'translate(-50%,-50%)',
+  transform: 'translate(-50%,-50%)'
 };
 
 const image1 =
-  'https://assets.trabiancdn.com/api/file/29QNOSugRqICIdZkNEBM+cu_101.png/convert?format=jpg&quality=85&compress=true&w=1500&h=600&fit=crop&align=faces'
+  'https://assets.trabiancdn.com/api/file/29QNOSugRqICIdZkNEBM+cu_101.png/convert?format=jpg&quality=85&compress=true&w=1500&h=600&fit=crop&align=faces';
 
-const image2 =
-  'http://bgfons.com/uploads/light/light_texture2224.jpg';
+const image2 = 'http://bgfons.com/uploads/light/light_texture2224.jpg';
 
 class Landing extends Component {
-
   constructor() {
     super();
     this.myRef = React.createRef();
   }
-  
+
   scroll(ref) {
-    ref.current.scrollIntoView({behavior: 'smooth'});
+    ref.current.scrollIntoView({ behavior: 'smooth' });
   }
 
-  render(){
+  render() {
     return (
       <div className='Landing-Page'>
-        <div id='header'><h1 id="logo">goodDeeds</h1></div>
-  
+        <div id='header'>
+          <h1 id='logo'>goodDeeds</h1>
+        </div>
+
         <Parallax bgImage={image1} strength={500}>
-          <div style={{ height: '100vh' }} >
+          <div style={{ height: '100vh' }}>
             <Row id='top-buffer' />
             <div id='info-overlay'>
               {/* <Card> */}
-              <h3 id='motto'>
-                "Where good deeds meet real needs"
-              </h3>
+              <h3 id='motto'>Where good deeds meet real needs.</h3>
             </div>
             <Row id='scroll-button-row'>
-              <Button floating id='scroll-button' waves='light' icon='keyboard_arrow_down' onClick={() => this.scroll(this.myRef)}></Button>
+              <Button
+                floating
+                id='scroll-button'
+                waves='light'
+                icon='keyboard_arrow_down'
+                onClick={() => this.scroll(this.myRef)}
+              />
             </Row>
-
           </div>
         </Parallax>
         <Parallax
-         
           bgImage={image2}
           strength={400}
           renderLayer={percentage => (
@@ -75,7 +76,7 @@ class Landing extends Component {
               <Login />
             </div>
           </div>
-          <div ref={this.myRef}></div>
+          <div ref={this.myRef} />
         </Parallax>
       </div>
     );
